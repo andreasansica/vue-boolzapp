e522341d3b456ae1c61b050b4b154b8baeb2d1c2
andreasansica <sansica.andrea99@gmail.com>
2020-11-23 17:41:51 +0100

    .

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var app = new Vue({
   el: "#app",
   data:{
-
+    active: 0,
     utente:{
       immagine: "img/avatar_5.jpg",
       nome: "Andrea"
@@ -18,16 +18,16 @@ var app = new Vue({
         ultimo_accesso:"20 11 2020 13:52:38",
         messaggi: [
           {
-            text: "Lo sai che ho aperto una nuova pizzeria?"
+            text_send: "Lo sai che ho aperto una nuova pizzeria?"
           },
           {
-            text : "Si, ma preferirei andare al cinema"
+            text_rec : "Si, ma preferirei andare al cinema"
           },
           {
-            text : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            text_send : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           },
           {
-            text : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            text_rec : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
           }
         ]
       },
@@ -40,13 +40,13 @@ var app = new Vue({
         ultimo_accesso:"20 11 2020 10:55:38",
         messaggi: [
           {
-            text: "Ciao come stai?"
+            text_send: "Ciao come stai?"
           },
           {
-            text : "Bene te?"
+            text_rec : "Bene te?"
           },
           {
-            text : "Bene"
+            text_send : "Bene"
           }
         ]
       },
@@ -59,10 +59,10 @@ var app = new Vue({
         ultimo_accesso:"20 11 2020 09:12:28",
         messaggi: [
           {
-            text: "Hei"
+            text_send: "Hei"
           },
           {
-            text : "Hei"
+            text_rec : "Hei"
           },
         ]
       },
@@ -76,14 +76,19 @@ var app = new Vue({
         ultimo_accesso:"20 11 2020 08:32:38",
         messaggi: [
           {
-            text: "Ciao"
+            text_rec: "Ciao"
           },
           {
-            text : "Ciao"
-          },
+            text_send : "Ciao"
+          }
         ]
       }
     ]
+  },
+  methods:{
+    chatActive : function(i){
+      this.active = i
+    }
   }
 
 
